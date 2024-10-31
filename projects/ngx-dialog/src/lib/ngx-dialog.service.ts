d6012f85387ed90ec195bf20b0f8b0159f684ab0
ko1ebayev <ko1ebayev.worx@gmail.com>
2024-10-31 22:48:40 +0500

    get rid of overload signatures

--- projects/ngx-dialog/src/lib/ngx-dialog.service.ts
+++ projects/ngx-dialog/src/lib/ngx-dialog.service.ts
@@ -33,136 +33,51 @@ export class NgxDialogService {
   private readonly document = inject(DOCUMENT);
 
   openDialog<Result>(
-    templateRef: TemplateRef<unknown>,
-    config?: DialogConfig,
-    data?: DialogData
-  ): Observable<Result>;
-  openDialog<Result>(
-    component: ComponentType<unknown>,
-    config?: DialogConfig,
-    data?: DialogData
-  ): Observable<Result>;
-  openDialog<Result>(
-    componentOrTemplateRef: ComponentType<unknown> | TemplateRef<unknown>,
-    configOrData?: DialogConfig | DialogData,
-    data?: DialogData
-  ): Observable<Result>;
-  openDialog<Result>(
-    componentOrTemplateRef: ComponentType<unknown> | TemplateRef<unknown>,
-    configOrData?: DialogConfig | DialogData,
-    data?: DialogData
+    templateOrComponent: ComponentType<any> | TemplateRef<any>,
+    dataAndConfig?: { data?: DialogData; config?: DialogConfig }
   ): Observable<Result> {
-    let config: DialogConfig | undefined;
-    let dialogData: DialogData | undefined;
+    const { data = {}, config = {} }= dataAndConfig || {};
+    const normalizedConfig = this.normalizeConfig(config);
 
-    if (configOrData) {
-      if (this.isConfig(configOrData)) {
-        config = configOrData;
-        dialogData = data;
-      } else {
-        config = undefined;
-        dialogData = configOrData;
-      }
+    const dialogRef = this.createDialogRef<Result>(normalizedConfig);
+
+    const injector = createDialogInjector(this.parentInjector, {
+      dialogRef: <DialogRef<unknown>>dialogRef,
+      dialogConfig: normalizedConfig,
+      data: data,
+    });
+
+    const ngxDialogHostRef = createComponent(NgxDialogHostComponent, {
+      environmentInjector: this.appRef.injector,
+      elementInjector: injector,
+    });
+
+    this.appRef.attachView(ngxDialogHostRef.hostView);
+
+    this.document
+      .getElementById(this.ngxDialog.hostID)!
+      .appendChild(dialogRef.nativeDialog);
+
+    dialogRef.nativeDialog.appendChild(
+      this.getComponentRootNode(ngxDialogHostRef)
+    );
+
+    if (templateOrComponent instanceof TemplateRef) {
+      ngxDialogHostRef.instance.contentInsertionPoint.viewContainerRef.createEmbeddedView(
+        templateOrComponent,
+        {
+          $implicit: dialogRef,
+          data,
+          config,
+          injector,
+        }
+      );
+    } else {
+      ngxDialogHostRef.instance.contentInsertionPoint.viewContainerRef.createComponent(
+        templateOrComponent,
+        { injector }
+      );
     }
-
-    return componentOrTemplateRef instanceof TemplateRef
-      ? this._openDialogFromTemplate<Result>(
-          componentOrTemplateRef,
-          config,
-          dialogData
-        )
-      : this._openDialogFromComponent<Result>(
-          componentOrTemplateRef,
-          config,
-          dialogData
-        );
-  }
-
-  private _openDialogFromTemplate<Result>(
-    templateRef: TemplateRef<unknown>,
-    config?: DialogConfig,
-    data?: DialogData
-  ): Observable<Result> {
-    const normalizedConfig = this.normalizeConfig(config || {});
-
-    const dialogRef = this.createDialogRef<Result>(normalizedConfig);
-
-    const injector = createDialogInjector(this.parentInjector, {
-      dialogRef: <DialogRef<unknown>>dialogRef,
-      dialogConfig: normalizedConfig,
-      data: data || {},
-    });
-
-    const ngxDialogHostRef = createComponent(NgxDialogHostComponent, {
-      environmentInjector: this.appRef.injector,
-      elementInjector: injector,
-    });
-
-    this.appRef.attachView(ngxDialogHostRef.hostView);
-
-    this.document
-      .getElementById(this.ngxDialog.hostID)!
-      .appendChild(dialogRef.nativeDialog);
-
-    dialogRef.nativeDialog.appendChild(
-      this.getComponentRootNode(ngxDialogHostRef)
-    );
-
-    const evr = ngxDialogHostRef.instance.contentInsertionPoint.viewContainerRef.createEmbeddedView(
-      templateRef,
-      {
-        $implicit: dialogRef,
-        data,
-        config,
-        injector,
-      }
-    );
-    console.log(evr);
-
-    dialogRef.nativeDialog.showModal();
-
-    return dialogRef.closed$.pipe(
-      take(1),
-      finalize(() => {
-        this.destroyDialog(ngxDialogHostRef, dialogRef.dialogID);
-      })
-    );
-  }
-
-  private _openDialogFromComponent<Result>(
-    component: ComponentType,
-    config?: DialogConfig,
-    data?: DialogData
-  ): Observable<Result> {
-    const normalizedConfig = this.normalizeConfig(config || {});
-
-    const dialogRef = this.createDialogRef<Result>(normalizedConfig);
-
-    const injector = createDialogInjector(this.parentInjector, {
-      dialogRef: <DialogRef<unknown>>dialogRef,
-      dialogConfig: normalizedConfig,
-      data: data || {},
-    });
-
-    const ngxDialogHostRef = createComponent(NgxDialogHostComponent, {
-      environmentInjector: this.appRef.injector,
-      elementInjector: injector,
-    });
-
-    this.appRef.attachView(ngxDialogHostRef.hostView);
-
-    this.document
-      .getElementById(this.ngxDialog.hostID)!
-      .appendChild(dialogRef.nativeDialog);
-
-    dialogRef.nativeDialog.appendChild(
-      this.getComponentRootNode(ngxDialogHostRef)
-    );
-
-    ngxDialogHostRef.instance.contentInsertionPoint.viewContainerRef.createComponent(
-      component,
-      { injector }
-    );
 
     dialogRef.nativeDialog.showModal();
 
@@ -202,10 +117,6 @@ export class NgxDialogService {
   private getComponentRootNode(componentRef: ComponentRef<any>): HTMLElement {
     return (componentRef.hostView as EmbeddedViewRef<any>)
       .rootNodes[0] as HTMLElement;
-  }
-
-  private isConfig(obj: object): obj is DialogConfig {
-    return obj && ('closeOnBackdropClick' in obj || 'htmlDialogClass' in obj);
   }
 
   private normalizeConfig(config: Partial<DialogConfig>): DialogConfig {
